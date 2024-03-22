da5407889bc79e32c2389e049d7f54042321e1d5
evwake <evwake259@gmail.com>
2024-03-22 11:53:44 -0400

    A8

---
+++ assignments/Kmeans/my_KMeans.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+
+class my_KMeans:
+
+    def __init__(self, n_clusters=5, n_init = 10, max_iter=300, tol=1e-4):
+        # use euclidean distance for sse calculation.
+        # stop when either # iteration is greater than max_iter or the delta of self.sse_ is smaller than tol.
+        # repeat n_init times and keep the best run (cluster_centers_, sse_) with the lowest sse_.
+        self.n_clusters = int(n_clusters)
+        self.n_init = n_init
+        self.max_iter = int(max_iter)
+        self.tol = tol
+
+        self.classes_ = range(n_clusters)
+        # Centroids
+        self.cluster_centers_ = None
+        # Sum of squared distances of samples to their closest cluster center.
+        self.sse_ = None
+
+    def pick_centers(self, X):
+        return X[np.random.choice(np.arange(len(X)), size=self.n_clusters, replace=False)]
+    
+
+    def dist(self, a, b):
+        # Compute Euclidean distance between a and b
+        return np.sum((np.array(a)-np.array(b))**2)**(0.5)
+    
+    def fit_once(self, X):
+        # Fit once
+        # Input X is numpy.array
+        # Output: cluster_centers (list), sse
+
+        # Initiate cluster centers
+        cluster_centers = self.pick_centers(X)
+        last_sse = None
+        # Iterate
+        for i in range(self.max_iter+1):
+            # Assign each training data point to its nearest cluster_centers
+            clusters = [[] for i in range(self.n_clusters)]
+            sse = 0
+            for x in X:
+                # calculate distances between x and each cluster center
+                dists = [self.dist(x, center) for center in cluster_centers]
+                # calculate sse
+                cluster_id = 0
+                min_dist = None
+                for i, dist in enumerate(dists):
+                    if min_dist is None or dist < min_dist:
+                        cluster_id = i
+                        min_dist = dist
+                # find the cluster that x belongs to
+                sse += dists[cluster_id] ** 2
+                
+
+                # add x to that cluster
+                clusters[cluster_id].append(x)
+
+            if (last_sse and last_sse - sse < self.tol) or i==self.max_iter:
+                break
+            
+            # Update cluster centers
+            cluster_centers = []
+            for idx in range(self.n_clusters):
+                cluster_avg = [0, 0, 0, 0]
+                for arr in clusters[idx]:
+                    for i, x in enumerate(arr):
+                        cluster_avg[i] += x
+                cluster_avg = [x / len(clusters[idx]) for x in cluster_avg]
+                cluster_centers.append(cluster_avg)
+            last_sse = sse
+
+        return cluster_centers, sse
+        
+
+    def fit(self, X):
+        # X: pd.DataFrame, independent variables, float
+        # repeat self.n_init times and keep the best run
+            # (self.cluster_centers_, self.sse_) with the lowest self.sse_.
+        X_feature = X.to_numpy()
+        for i in range(self.n_init):
+            cluster_centers, sse = self.fit_once(X_feature)
+            if self.sse_==None or sse < self.sse_:
+                self.sse_ = sse
+                self.cluster_centers_ = cluster_centers
+        return
+
+    def transform(self, X):
+        # Transform to cluster-distance space
+        # X: pd.DataFrame, independent variables, float
+        # return dists = list of [dist to centroid 1, dist to centroid 2, ...]
+        dists = [[self.dist(x,centroid) for centroid in self.cluster_centers_] for x in X.to_numpy()]
+        return dists
+
+    def predict(self, X):
+        # X: pd.DataFrame, independent variables, float
+        # return predictions: list
+        predictions = [np.argmin(dist) for dist in self.transform(X)]
+        return predictions
+
+    def fit_predict(self, X):
+        self.fit(X)
+        return self.predict(X)
